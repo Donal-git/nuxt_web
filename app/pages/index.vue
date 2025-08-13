5dc6e0c5cf4aa541bfcaf96e2ab0e5f58098b7ea
Donal-git <pagnagdoued@gmail.com>
2025-08-13 12:48:26 +0000

    modification de la page index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,7 +6,7 @@
     <div style="font-family: sans-serif; text-align: center; margin-top: 50px;">
         <h1>Compteur : {{ counter.count }}</h1>
         <button @click="counter.increment">Incrémenter (+1)</button>
-        <button @click="counter.increment">Décrémenter (-1)</button>
+        <button @click="counter.decrement">Décrémenter (-1)</button>
     </div>
 </template>
 
